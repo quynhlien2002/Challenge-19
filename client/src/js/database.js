e9f6751bbdc85d3c15004db08fd32b2d3810c233
Daisy Lien <daisylien1211@gmail.com>
2022-11-14 18:19:37 -0800

    Pushing to Heroku

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -30,7 +30,7 @@ console.log('GET from the database');
 const jateDb = await openDB('jate', 1);
 const tx = jateDb.transaction('jate', 'readonly');
 const store = tx.objectStore('jate');
-const request = store.getAll();
+const request = store.get(1);
 const result = await request;
 console.log('result.value', result);
 return result;
